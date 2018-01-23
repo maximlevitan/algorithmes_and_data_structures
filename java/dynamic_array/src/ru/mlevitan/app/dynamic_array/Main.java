a82fa288e2e6e0e012d5c3ec426ef7005f35385c
Maxim Levitan <maximlevitan@gmail.com>
2018-01-24 00:14:09 +0300

    add select sort and insert sort

--- java/dynamic_array/src/ru/mlevitan/app/dynamic_array/Main.java
+++ java/dynamic_array/src/ru/mlevitan/app/dynamic_array/Main.java
@@ -1,17 +1,43 @@
 package ru.mlevitan.app.dynamic_array;
 
+/**
+ * - Создать массив большого размера (миллион элементов).
+ * - Написать методы удаления, добавления, поиска элемента массива.
+ * - Заполнить массив случайными числами.
+ * - Написать методы, сортировок и вывести на экран скорость выполнения каждой, используя метод System.nanoTime().
+ */
 public class Main {
 
     public static void main(String[] args) {
+        int testArraySize = 100000;
+
         DynamicArray<Integer> test = new DynamicArray<>();
 
-        test.randomFill(1000000, 1, 10000);
+        test.randomFill(100000, 1, 10000);
 
-        SimpleSort.bubbleSort(test.toArray());
+        // Сортировка пузырьком 100К элементов выполнялась 29251141465 наносекунд
+        SimpleSort.startTimer();
+        SimpleSort.sortBubble(test.toArray());
+        System.out.printf("Bubble sort array N = %d time (nanosec.): %d",
+                testArraySize, SimpleSort.getResultTimeDiff());
 
-        System.out.println(test.size());
+        test = new DynamicArray<>();
+        test.randomFill(100000, 1, 10000);
 
-        //System.out.println(test);
+        // Сортировка выбором 100К элементов выполнялась 9671751959 наносекунд
+        SimpleSort.startTimer();
+        SimpleSort.sortSelect(test.toArray());
+        System.out.printf("Select sort array N = %d time (nanosec.): %d",
+                testArraySize, SimpleSort.getResultTimeDiff());
+
+        test = new DynamicArray<>();
+        test.randomFill(100000, 1, 10000);
+
+        // Сортировка вставками 100К элементов выполнялась 7708532459 наносекунд
+        SimpleSort.startTimer();
+        SimpleSort.sortInsert(test.toArray());
+        System.out.printf("Insert sort array N = %d time (nanosec.): %d",
+                testArraySize, SimpleSort.getResultTimeDiff());
     }
 
 }
--- java/dynamic_array/src/ru/mlevitan/app/dynamic_array/SimpleSort.java
+++ java/dynamic_array/src/ru/mlevitan/app/dynamic_array/SimpleSort.java
@@ -2,23 +2,74 @@ package ru.mlevitan.app.dynamic_array;
 
 public class SimpleSort {
 
-    public static <T extends Object & Comparable<T>> void bubbleSort(T[] array) {
+    private static long startedNanoTime;
+
+    public static <T extends Object & Comparable<T>> void sortBubble(T[] array) {
         int size = array.length;
+        int j;
 
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                compareAndSwap(array, i, j);
+            for (j = i; j < size - 1; j++) {
+                compareAndSwap(array, i, j + 1);
             }
         }
     }
 
+    public static <T extends Object & Comparable<T>> void sortSelect(T[] array) {
+        int size = array.length;
+        int mark;
+        int j;
+
+        for (int i = 0; i < size - 1; i++) {
+            mark = i;
+            for (j = i + 1; j < size; j++) {
+                if (array[j] != null && array[mark] != null && array[j].compareTo(array[mark]) < 0) {
+                    mark = j;
+                }
+            }
+
+            swap(array, i, mark);
+        }
+    }
+
+    public static <T extends Object & Comparable<T>> void sortInsert(T[] array) {
+        int size = array.length;
+        int j;
+        T temp;
+
+        for (int i = 1; i < size; i++) {
+            if (array[i] == null || array[i - 1] == null) {
+                continue;
+            }
+
+            temp = array[i];
+            for (j = i; j > 0 && array[j - 1].compareTo(temp) > 0; j--) {
+                array[j] = array[j - 1];
+            }
+
+            array[j] = temp;
+        }
+    }
+
+    public static void startTimer() {
+        startedNanoTime = System.nanoTime();
+    }
+
+    public static long getResultTimeDiff() {
+        return System.nanoTime() - startedNanoTime;
+    }
+
     private static <T extends Object & Comparable<T>> void compareAndSwap(T[] array, int index1, int index2) {
-        if (array[index2] == null
-            || array[index1] != null && array[index2] != null && array[index1].compareTo(array[index2]) < 0
-        ) {
+        if (array[index1] != null && array[index2] != null && array[index1].compareTo(array[index2]) > 0) {
             T temp = array[index1];
             array[index1] = array[index2];
             array[index2] = temp;
         }
+    }
+
+    private static <T extends Object> void swap(T[] array, int index1, int index2) {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
